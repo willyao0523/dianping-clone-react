b88e5905cdecee2f0056828da88ea096e6d76dbf
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-02-21 19:50:35 +1100

    fix the style problem

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import App from './containers/APP';
 import store from "./redux/store";
+import "./index.css"
 
 ReactDOM.render(
   <Provider store={store}>
