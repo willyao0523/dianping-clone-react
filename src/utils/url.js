8f626caff6475afb5e15c09b71176e543afd929f
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-02-23 00:33:05 +1100

    fix bugs

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  getProductList: (rowIndex, pageSize) => `/mock/products/likes.json?rowIndex=${rowIndex}&pageSize=${pageSize}`,
+  getProductList: (path, rowIndex, pageSize) => `/mock/products/${path}.json?rowIndex=${rowIndex}&pageSize=${pageSize}`,
 }
