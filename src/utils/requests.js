8d3dc0e11dcc85d3f2a4bfa6da7145dde803e6b2
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-02-23 00:34:52 +1100

    fix bugs

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,7 +1,7 @@
 const headers = new Headers({
   "Accept": "application/json",
   "Content-Type": "application/json"
-});
+})
 
 function get(url) {
   return fetch(url, {
@@ -9,32 +9,32 @@ function get(url) {
     headers: headers
   }).then(response => {
     return handleResponse(url, response);
-  }).catch(err => {
-    console.error(`Request failed. Url= ${url}. Message=${err}`)
-    return Promise.reject({error: {message: "Request failed"}});
+  }).catch(error => {
+    console.error(`Request failed. Url = ${url}. Message = ${error}`)
+    return Promise.reject({error: {message: "Request failed."}})
   })
 }
 
 function post(url, data) {
   return fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: headers,
     body: data
   }).then(response => {
     return handleResponse(url, response);
-  }).catch(err => {
-    console.error(`Request failed. Url= ${url}. Message=${err}`)
-    return Promise.reject({error: {message: "Request failed"}});
+  }).catch(error => {
+    console.error(`Request failed. Url = ${url}. Message = ${error}`)
+    return Promise.reject({error: {message: "Request failed."}})
   })
 }
 
 function handleResponse(url, response) {
-  if(response.statusCode === 200) {
+  if(response.status === 200) {
     return response.json();
   } else {
-    console.error(`Request failed. Url= ${url}`)
-    return Promise.reject({error: {message: "Request failed due to server error"}});
+    console.error(`Request failed. Url = ${url}`)
+    return Promise.reject({error: {message: "Request failed due to server error"}})
   }
 }
 
-export { get, post }
+export {get, post}
