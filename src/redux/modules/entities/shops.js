1b5ed9a4ff4d48486f5158e26a7ff87dcaf76137
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-02-26 00:17:30 +1100

    add shops redux

--- src/redux/modules/entities/shops.js
+++ src/redux/modules/entities/shops.js
@@ -1,5 +1,16 @@
-const reducer = (state = {}, action) => {
-  return state;
+import createReducer from '../../../utils/createReducer'
+
+const schema = {
+  name: "shops",
+  id: 'id'
 }
 
+const reducer = createReducer(schema.name)
+
 export default reducer;
+
+// selector
+export const getShopById = (state, id) => {
+  const shop = state.entities.shop[id]
+  return shop
+}
