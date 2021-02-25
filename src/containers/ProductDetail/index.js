d5164ed03d5b3f6937162b7fc6e3ae1dd98f4137
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-02-25 23:42:44 +1100

    load other components

--- src/containers/ProductDetail/index.js
+++ src/containers/ProductDetail/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header'
 import ProductOverview from './components/ProductOverview'
 import ShopInfo from './components/ShopInfo'
 import Detail from './components/Detail'
+import Remark from './components/Remark'
+import BuyButton from './components/BuyButton'
 
 class ProductDetail extends Component {
   render() {
     return (
       <div>
+        <Header title="团购详情" onBack={this.handleBack} grey />
         <ProductOverview />
         <ShopInfo />
         <Detail />
+        <Remark />
+        <BuyButton />
       </div>
     );
   }
+
+  handleBack = () => {
+
+  }
+
 }
 
 export default ProductDetail;
